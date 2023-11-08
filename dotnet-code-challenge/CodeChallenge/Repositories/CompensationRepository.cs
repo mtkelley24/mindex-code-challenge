b1e505a61e2956a156d93edfd4379ef77fdcf1df
mtkelley24 <98991890+mtkelley24@users.noreply.github.com>
2023-11-07 20:58:51 -0600

    Update CompensationRepository.cs

--- dotnet-code-challenge/CodeChallenge/Repositories/CompensationRepository.cs
+++ dotnet-code-challenge/CodeChallenge/Repositories/CompensationRepository.cs
@@ -18,26 +18,31 @@ namespace CodeChallenge.Repositories
 
 		public Compensation Add(Compensation compensation)
 		{
+  			// get employee from employee context. 
             		compensation.Employee = _employeeContext
                 		.Employees
                 		.FirstOrDefault(i => i.EmployeeId == compensation.Employee.EmployeeId);
 
-            		compensation.CompensationId = Guid.NewGuid().ToString();
+            		//Set compensation id key
+	      		compensation.CompensationId = Guid.NewGuid().ToString();
+
+  			//Add to compensations
 			_employeeContext.Compensations.Add(compensation);
 			return compensation;
 		}
 
 		public Compensation GetCompensationById(string id)
 		{
+  			//match compensation using employee id
 			return _employeeContext.Compensations
 				.Include(c => c.Employee)
 				.FirstOrDefault(c => c.Employee.EmployeeId == id);
 		}
 
-        public Task SaveAsync()
-        {
-            return _employeeContext.SaveChangesAsync();
-        }
+        	public Task SaveAsync()
+       		{
+            		return _employeeContext.SaveChangesAsync();
+        	}
     }
 }
 
